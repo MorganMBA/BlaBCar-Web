e9ea1cbe57193efcfa707dbfa1b6068b8862ec31
MorganMBA <mba@intechinfo.fr>
2020-04-26 01:25:41 +0200

    value component

--- BlaBCar-SPA/src/app/app.module.ts
+++ BlaBCar-SPA/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TrajetComponent } from './trajet/trajet.component';
+import { ValueComponent } from './value/value.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrajetComponent
+    TrajetComponent,
+    ValueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
