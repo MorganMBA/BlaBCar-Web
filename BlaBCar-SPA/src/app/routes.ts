4ea49c691c88aa1a488151baceefdeff1accb2bd
MorganMBA <mba@intechinfo.fr>
2020-05-14 09:38:01 +0200

    adding routes with guard

--- BlaBCar-SPA/src/app/routes.ts
+++ BlaBCar-SPA/src/app/routes.ts
@@ -3,11 +3,19 @@ import { HomeComponent } from './home/home.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { ProposerComponent } from './trajet/proposer/proposer.component';
 import { RechercherComponent } from './trajet/rechercher/rechercher.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'reservations', component: ReservationComponent},
-  {path: 'proposer-trajet', component: ProposerComponent},
-  {path: 'rechercher-trajet', component: RechercherComponent},
-  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+  {path: '', component: HomeComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'reservations', component: ReservationComponent},
+      {path: 'proposer-trajet', component: ProposerComponent},
+      {path: 'rechercher-trajet', component: RechercherComponent}
+    ]
+  },
+  {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
